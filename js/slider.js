9dfd454fa1e11e76ff3ae1aa31378cbdf52c9bb1
Frank Lang <frank.lang@netapsys.fr>
2018-03-15 14:28:53 +0100

    Le thumb pager se replace desormais automatiquement sur le slider qui contient le visuel actif.

--- js/slider.js
+++ js/slider.js
@@ -25,7 +25,7 @@ ACC.slider = {
     triggerNextValues: null,
     sliderThumb: null,
         sliderThumbMinSlides: 6,
-        sliderThumbModalMinSlides: 9,
+        sliderThumbModalMinSlides: 6,
     sliderMain: null,
 
     sliderConfig:{
@@ -36,7 +36,8 @@ ACC.slider = {
             slideMargin: 10,
             infiniteLoop: false,
             hideControlOnEnd: true,
-            pager: false
+            pager: false,
+            startSlide: 0
         },
         "main":{
             pagerCustom: '#sliderThumb',
@@ -109,25 +110,36 @@ ACC.slider = {
             $('#triggerModal').hide();
             $('#sliders').appendTo('#modalSliders');
 
-            ACC.slider.sliderConfig.thumb.minSlides = ACC.slider.sliderThumbModalMinSlides;
-            // ACC.slider.sliderConfig.thumb.startSlide = ACC.slider.newIndex;
-
-            ACC.slider.sliderThumb.reloadSlider(ACC.slider.sliderConfig.thumb);
-            ACC.slider.handlePrevNextControls();
-
             ACC.slider.triggerWindowResizeEvent();
         }).on('hidden.bs.modal', function(){
             $('#triggerModal').show();
             $('#sliders').appendTo('#pageSliders');
 
-            ACC.slider.sliderConfig.thumb.minSlides = ACC.slider.sliderThumbMinSlides;
-            // ACC.slider.sliderConfig.thumb.startSlide = ACC.slider.newIndex;
-
             ACC.slider.sliderThumb.reloadSlider(ACC.slider.sliderConfig.thumb);
-            ACC.slider.handlePrevNextControls();
+            ACC.slider.sliderThumb.goToSlide(ACC.slider.triggerPrevValues.findIndex(ACC.slider.arrayFindIndex));
+            console.log('closest value out of array: ' + ACC.slider.getClosestNumberOutOfArray(ACC.slider.newIndex, ACC.slider.triggerPrevValues));
+            console.log('index of that value in array: ' + ACC.slider.triggerPrevValues.findIndex(ACC.slider.arrayFindIndex));
 
             ACC.slider.triggerWindowResizeEvent();
         });
+    },
+
+    getClosestNumberOutOfArray: function(num, arr){
+        // https://stackoverflow.com/questions/8584902/get-closest-number-out-of-array
+        var curr = arr[0];
+        var diff = Math.abs (num - curr);
+        for (var val = 0; val < arr.length; val++) {
+            var newdiff = Math.abs (num - arr[val]);
+            if (newdiff < diff) {
+                diff = newdiff;
+                curr = arr[val];
+            }
+        }
+        return curr;
+    },
+
+    arrayFindIndex: function(elem){
+      return elem == ACC.slider.getClosestNumberOutOfArray(ACC.slider.newIndex, ACC.slider.triggerPrevValues);
     },
 
     triggerWindowResizeEvent: function(){
@@ -136,35 +148,28 @@ ACC.slider = {
         window.dispatchEvent(evt);
     },
 
-    // getViewportHeight: function(){
-    //     var viewportHeight = $(window).height();
-    // },
-
-    // onWindowResize: function(){
-    //     $(window).on('resize', function(){
-    //         var viewportHeight = $(window).height();
-    //         console.log(viewportHeight);
-    //     });
-    // },
-
     debugInfo: function(){
       $('body').append('<div id="debugInfo" />');
       $('#debugInfo').append(
            '<ul>'
           +'    <li>Slides in main slider: <strong id="mainSlideCount"></strong></li>'
           +'    <li>Slides in thumb slider: <strong id="thumbSlideCount"></strong></li>'
-          +'    <li>Active slide: <strong id="newIndex"></strong></li>'
+          +'    <li>Active image: <strong id="newIndex"></strong></li>'
+          +'    <li>Active slide in main slider: <strong id="activeSlideInMainSlider"></strong></li>'
+          +'    <li>Active slide in thumb slider: <strong id="activeSlideInThumbSlider"></strong></li>'
           +'    <li>Trigger previous Thumb slider slide when active value is: <strong id="triggerPrevValues"></strong></li>'
           +'    <li>Trigger next Thumb slider slide when active value is: <strong id="triggerNextValues"></strong></li>'
           +'</ul>');
 
       $('#mainSlideCount').text(this.mainSlideCount);
       $('#thumbSlideCount').text(this.thumbSlideCount);
-      $('#newIndex').text(+this.newIndex);
+      $('#newIndex, #activeSlideInMainSlider').text(+this.newIndex);
+      $('#activeSlideInThumbSlider').text(ACC.slider.triggerPrevValues.findIndex(ACC.slider.arrayFindIndex));
       $('#triggerPrevValues').text(ACC.slider.triggerPrevValues);
       $('#triggerNextValues').text(ACC.slider.triggerNextValues);
       $(document).on('click', ['.bx-prev', '.bx-next'], function(e) {
-          $('#newIndex').text(ACC.slider.newIndex);
+          $('#newIndex, #activeSlideInMainSlider').text(ACC.slider.newIndex);
+          $('#activeSlideInThumbSlider').text(ACC.slider.triggerPrevValues.findIndex(ACC.slider.arrayFindIndex));
       });
       $('#zoomModal').on('shown.bs.modal', function(){
           $('#thumbSlideCount').text(ACC.slider.thumbSlideCount);
@@ -172,6 +177,7 @@ ACC.slider = {
           $('#triggerNextValues').text(ACC.slider.triggerNextValues);
       }).on('hidden.bs.modal', function(){
           $('#thumbSlideCount').text(ACC.slider.thumbSlideCount);
+          $('#activeSlideInThumbSlider').text(ACC.slider.triggerPrevValues.findIndex(ACC.slider.arrayFindIndex));
           $('#triggerPrevValues').text(ACC.slider.triggerPrevValues);
           $('#triggerNextValues').text(ACC.slider.triggerNextValues);
       });
